fd81866ab873274f82c6a5fe20f5846b959a40ad
Brad Sing <bradanddebbs@gmail.com>
2026-03-14 11:33:04 +1000

    use HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from "./components/Header"
 import Main from "./components/Main"
 import Footer from "./components/Footer"
 import NumberDetails from "./components/NumberDetails"
-import { Routes, Route } from "react-router-dom"
-
-import "./styles/general.css"
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/number/:numberId" element={<NumberDetails />} />
+        <Route path="/number/:id" element={<NumberDetails />} />
       </Routes>
       <Footer />
-    </>
+    </Router>
   )
 }
 
-export default App;
+export default App
